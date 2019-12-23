13270c16a4876edcc63cde0b2aa1ac936c957111
Sarfraz Ahmed <sarfraznawaz2005@gmail.com>
2019-12-23 22:02:16 +0500

    update

--- src/Outputs/Web.php
+++ src/Outputs/Web.php
@@ -63,7 +63,7 @@ class Web implements Output
                  html { font-size: 1rem !important; }
                 .indexer_query_info, .indexer_query_info:active .indexer_query_info:visited .indexer_query_info:hover { position:fixed !important; z-index:2147483647 !important; bottom:20px !important; right:45px !important; padding: 2px 10px 5px 10px !important; font-size:20px !important; border-radius:5px !important;color:#333 !important; text-decoration: none !important; }
                 .indexer_query_info .number { font-weight: bold !important; font-size: 24px !important; }
-                .indexer_alert { background: #a1ff8e !important; padding:2px 5px !important; border-radius: 5px !important; position:fixed !important; z-index:2147483647 !important; bottom:65px !important; right:45px !important;}
+                .indexer_alert { background: #a1ff8e !important; padding:2px 5px !important; border-radius: 5px !important; position:fixed !important; z-index:2147483647 !important; bottom:70px !important; right:45px !important;}
                 .indexer_small { font-size: .90rem !important;}
                 .indexer pre { background: #fff !important; color:#000 !important; padding:10px; !important; margin:0 !important; border: none !important; }
                 .indexer { width:100% !important; height:100% !important; position: fixed !important; background: #edf1f3 !important; top:0 !important; left:0 !important; color:#000 !important; padding:25px !important; z-index:999999999 !important; margin:0; overflow:auto; font-family: "Helvetica Neue", Helvetica, Arial, sans-serif !important; font-size:1rem !important; line-height: 1rem !important; }
